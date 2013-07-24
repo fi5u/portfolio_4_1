b76f1fcad81a68fd0ad3890ea1883a34c17e9d1e
Tommy Fisher <tommybfisher@gmail.com>
2013-07-24 15:38:02 +0300

    Styled single further, changed many button links to text links, added own lightbox

--- Build/assets/js/events.js
+++ Build/assets/js/events.js
@@ -9,3 +9,28 @@ $("textarea").focusin(function() {
 $("textarea").focusout(function() {
     toggleTextarea();
 });
+
+$("#imageblock").on("click", "a#imageblock-link", function(event) {
+    var url = $("#imageblock a").attr("href");
+    lightbox(url);
+});
+
+$("body").on("click", ".lightbox", function(event) {
+    $(".lightbox").fadeOut("fast");
+});
+
+
+$("#filmstrip").on("click", "a", function(event) {
+    event.preventDefault();
+
+    var large = $(this).attr("href");
+    var medium = $(this).children("img").attr("data-medium");
+
+    updateSingleView(medium, large);
+});
+
+$(window).resize(function() {
+    waitOnEvent(function() {
+        lightboxVCenter();
+    }, 500, "reset1");
+});
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,20 +9,75 @@ function toggleTextarea(activate) {
 }
 
 function bindFlexorder() {
-/*    $(".single__textblock").flexorder({
-        breakpoint: 768,
-        targetContainer: $(".single__filmstrip-contact")
-    });
-
-    $(".single__filmstrip").flexorder({
-        breakpoint: 768,
-        targetContainer: $(".single__imagetext"),
-        targetPosition: "end"
-    });*/
-
     $(".single__filmstrip").flexorder({
         breakpoint: 1024,
         targetContainer: $(".single__imageblock"),
         targetPosition: "end"
     });
 }
+
+function bindSvgeezy() {
+    svgeezy.init(false, 'png');
+}
+
+function lightboxVCenter() {
+    var winH = $(window).height();
+    var lbImgH = $(".lightbox__img").outerHeight();
+    var top = (winH - lbImgH) / 2;
+    $(".lightbox__content").css("top",top + "px");
+}
+
+function lightbox(url) {
+    if( $('.lightbox').length > 0 ) {
+        $('.lightbox__img').html('<img src="' + url + '">');
+        $('.lightbox').fadeIn("fast");
+    } else {
+        var lightbox =
+        '<div class="lightbox">' +
+            '<div class="lightbox__content">' +
+                '<div class="lightbox__img">' +
+                    '<img src="' + url +'">' +
+                '</div>' +
+                '<div class="lightbox__controls">' +
+                    '<div class="lightbox__control--left"></div>' +
+                    '<div class="lightbox__control--right"></div>' +
+                '</div>' +
+            '</div>' +
+        '</div>';
+        $('body').append(lightbox);
+    }
+    lightboxVCenter();
+    event.preventDefault();
+}
+
+function updateSingleView(medium, large) {
+    $("#imageblock-link").attr("href",large);
+    $("#imageblock-link img").attr("src", medium);
+}
+
+function preloadImages() {
+    if( $("#filmstrip a").length ) {
+        $("#filmstrip a").each(function() {
+            var urlLg = $(this).attr("href"),
+                urlMed = $(this).children(),
+                imgLg = new Image(),
+                imgMed = new Image();
+
+            imgLg.src = urlLg;
+            imgMed.src = urlLg;
+        });
+    }
+}
+
+var waitOnEvent = (function () {
+    var timers = {};
+    return function (callback, ms, uniqueId) {
+        if (!uniqueId) {
+            uniqueId = "1";
+        }
+        if (timers[uniqueId]) {
+            clearTimeout (timers[uniqueId]);
+        }
+        timers[uniqueId] = setTimeout(callback, ms);
+    };
+})();
