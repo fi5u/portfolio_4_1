ea708f59db4ea46b3a61ea0aa54a89d7ff91e5a3
Tommy Fisher <tommybfisher@gmail.com>
2013-07-25 16:44:19 +0300

    Revert "Changed bio to use inbuilt wordpress description"
    This reverts commit 3e2c275582564b50f2b7179c62959b8e70057b82.

--- Build/part-bio.php
+++ Build/part-bio.php
@@ -1,6 +1,4 @@
             <div class="bio">
                 <h2>bio</h2>
-                <?php if( get_the_author_meta('description') ) : ?>
-                    <?php echo get_the_author_meta('description'); ?>
-                <?php endif; ?>
+                <?php the_field("bio","option"); ?>
             </div>
